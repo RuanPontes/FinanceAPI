7a35928374911972b0bc469dfeec1eeee0c3c4d0
Ruan <ruanpontesfrazao@gmail.com>
2023-08-08 23:52:58 -0300

    primeiro commit

---
+++ model/src/main/java/com/indracompany/treinamento/exception/AplicacaoExceptionValidacoes.java
@@ -0,0 +1,57 @@
+package com.indracompany.treinamento.exception;
+
+/**
+ * @author efmendes
+ *
+ */
+@SuppressWarnings("JavadocReference")
+public interface AplicacaoExceptionValidacoes {
+
+  public static final Integer SEVERIDADE_ERRO = 1;
+
+  public static final Integer SEVERIDADE_ALERTA = 2;
+
+  /**
+   * Código que referência a Mensagem Principal encontrada no arquivo {@link messages.properties}
+   *
+   * @return String
+   */
+  @SuppressWarnings("JavadocReference")
+  public String getCodigoMsg();
+
+  /**
+   * Código que referência a Mensagem Auxiliar encontrada no arquivo {@link messages.properties}.
+   *
+   * @return String
+   */
+  @SuppressWarnings("JavadocReference")
+  public String getCodigoMsgAuxiliar();
+
+  /**
+   * O texto que representa a mensagem do codigo do arquivo {@link messages.properties}.
+   *
+   * @return String
+   */
+  @SuppressWarnings("JavadocReference")
+  public String getDescricaoMsg(String... params);
+
+  /**
+   * O texto que representa a mensagem Auxiliar do codigo do arquivo {@link messages.properties}.
+   *
+   * @return String
+   */
+  @SuppressWarnings("JavadocReference")
+  public String getDescricaoMsgAuxiliar(String... params);
+
+  /**
+   * Severidade dita a forma que será formatado o texto no sistema web.
+   *
+   * Caso não seja passado, por default a severidade é SEVERIDADE_ERRO (1).
+   *
+   * @return int
+   */
+  public Integer getSeveridade();
+
+
+
+}
